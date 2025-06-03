2dbd83cfa4fbd0c51a8585d66bf0cd3dfbe2f188
girishmohakar <girishmohakarentc@gmail.com>
2025-06-03 16:20:28 +0530

    Create Code.c

---
+++ Code.c
@@ -0,0 +1,20 @@
+<!DOCTYPE html>  <html>  
+<head>  
+<title>HELLO WORLD this is my first html project</title>  
+</head>  
+<body>  
+<h1>HELLO WORLD this is my first html project </h1>  
+ <b>name:</b> <input type="text" name="name"><br>  
+<b>dob:</b> <input type="date" name="dob"><br>  
+<b>adress:</b> <input type="text" name="adress"><br>  
+<b>phone number:</b> <input type="text" name="phone number"><br>  
+<b>age:</b> <input type="range" min="0" max="100" step="1" value="20" name="age"><br>  
+<b>age:</b> <input type="checkbox" value="yes" name="follow"><br>  
+<b>photo:</b> <input type="file" name="follow"><br>  
+<b>signiture:</b> <input type="file" name="follow"><br>  
+<div align="right">  
+<button>Submit</button>  
+</div>  
+</body>  
+</html>
+
